9b48ffe5c582e6eae66b84e957eaa32786a7373e
payreks <admin@payreks.com>
2021-09-22 19:08:58 +0300

    22.09.2021

--- index.php
+++ index.php
@@ -11,13 +11,23 @@ require_once "src/Invoices.php";
 try {
 	$parasutAuthorization = new \Parasut\API\Authorization([
 		"development" => false, //development mode
-		"client_id" => "X",
-		"client_secret" => "X",
-		"username" => "X",
-		"password" => "X",
+		"client_id" => "a6c74cc876fe40f3c1a3873a2d39b947d082b0811f465fd3b589cfb917d4deb2",
+		"client_secret" => "f7a5670f46978ba7722abe9144d644e0c06d059527449091874eb17b4d789c4c",
+		"username" => "admin@payreks.com",
+		"password" => "05372007042esaT?",
 		"redirect_uri" => "urn:ietf:wg:oauth:2.0:oob",
-		"company_id" => "XXXX"
+		"company_id" => "275851"
 	]);
+
+//	$parasutAuthorization = new \Parasut\API\Authorization([
+//		"development" => true, //development mode
+//		"client_id" => "5sH0vKRSxvx9BNiJj23jiHZliNnDVf5sVkUFkF6UWYk",
+//		"client_secret" => "EO5RSliBnX_OS7BLCDYEoMyhiCzvBTFGC7OhkFrzEoE",
+//		"username" => "admin@payreks.com",
+//		"password" => "05372007042esaT?",
+//		"redirect_uri" => "urn:ietf:wg:oauth:2.0:oob",
+//		"company_id" => "2287"
+//	]);
 } catch (\Parasut\API\Exception $e) {
 	echo "Error code : " . $e->getCode()."<br>";
 	echo "Error message : " . $e->getMessage();
@@ -35,15 +45,20 @@ try {
 
 $invoices = new \Parasut\API\Invoices($parasutAuthorization);
 
-$show = $invoices->list_e_invoices();
+$show = $invoices->list_e_invoices(2);
+
+$count = 0;
 foreach ($show->result->data as $invoice)
 {
-	var_dump($invoice);
-//	if ($invoice->attributes->direction == "inbound")
-//	{
-//		var_dump($invoices->pdf_e_invoice($invoice->id)->result->data->attributes->url);
-//	}
+	if ($invoice->attributes->direction == "inbound" && $invoice->id == "12386140")
+	{
+//		$count++;
+//		var_dump($invoice->id);
+		var_dump($invoices->pdf_e_invoice($invoice->id)->result->data->attributes->url);
+//		sleep(1);
+	}
 }
+var_dump($count);
 die;
 var_dump($invoices->pdf_e_invoice($show->data->relationships->active_e_document->data->id));
 
