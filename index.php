67b10a1c471ee206cbc3c65446fc2a0b20d63b18
payreks <admin@payreks.com>
2021-09-23 10:36:22 +0300

    23.09.2021

--- index.php
+++ index.php
@@ -9,25 +9,7 @@ require_once "src/Invoices.php";
 
 
 try {
-	$parasutAuthorization = new \Parasut\API\Authorization([
-		"development" => false, //development mode
-		"client_id" => "a6c74cc876fe40f3c1a3873a2d39b947d082b0811f465fd3b589cfb917d4deb2",
-		"client_secret" => "f7a5670f46978ba7722abe9144d644e0c06d059527449091874eb17b4d789c4c",
-		"username" => "admin@payreks.com",
-		"password" => "05372007042esaT?",
-		"redirect_uri" => "urn:ietf:wg:oauth:2.0:oob",
-		"company_id" => "275851"
-	]);
 
-//	$parasutAuthorization = new \Parasut\API\Authorization([
-//		"development" => true, //development mode
-//		"client_id" => "5sH0vKRSxvx9BNiJj23jiHZliNnDVf5sVkUFkF6UWYk",
-//		"client_secret" => "EO5RSliBnX_OS7BLCDYEoMyhiCzvBTFGC7OhkFrzEoE",
-//		"username" => "admin@payreks.com",
-//		"password" => "05372007042esaT?",
-//		"redirect_uri" => "urn:ietf:wg:oauth:2.0:oob",
-//		"company_id" => "2287"
-//	]);
 } catch (\Parasut\API\Exception $e) {
 	echo "Error code : " . $e->getCode()."<br>";
 	echo "Error message : " . $e->getMessage();
